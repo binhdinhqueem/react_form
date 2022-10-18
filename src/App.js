872226787335b9221be4b05d11af5be8e22bf017
HuynhMinhHoang <binhdinhqueem@gmail.com.vn>
2022-10-18 19:15:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TrangChuSanPham from './components/TrangChuSanPham';
+
+
+function App() {
+  return (
+    <TrangChuSanPham />
+  );
+}
+
+export default App;
